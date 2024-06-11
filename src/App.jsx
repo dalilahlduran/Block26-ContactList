afc375d28e820fab1316bdb23b3ca8da3d78012f
Dalilah Duran <dalilahduran@Dalilahs-iMac.local>
2024-06-10 20:50:13 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
+  return (
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
+    </>
+  );
+}
+<div>
 
-export default App
+</div>
+
